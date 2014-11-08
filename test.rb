e9b110eaecd29e37e0090094a6d7384a06d8c0ef
Markham <quinesquines@gmail.com>
2014-11-08 10:33:40 -0800

    Use spaces instead of tabs

--- test.rb
+++ test.rb
@@ -3,22 +3,22 @@ load 'heap_sort_demo.rb'
 
 # Function for executing an algorithm demo
 def profile name
-	num_array = @numbers_to_sort
-	start = Time.now
-	yield(num_array)
-	finish = Time.now
-	puts "%20s %20f" % [name, finish - start]	
+  num_array = @numbers_to_sort
+  start = Time.now
+  yield(num_array)
+  finish = Time.now
+  puts "%20s %20f" % [name, finish - start]
 end
 
 # Generate test data
 @numbers_to_sort = 
 if ARGV[0] && File.exist?(ARGV[0])
-	puts "Reading test data"
-	require 'json'
-	JSON.parse File.read(ARGV[0])
+  puts "Reading test data"
+  require 'json'
+  JSON.parse File.read(ARGV[0])
 else
-	puts "Generating test data"
-	(0..10000).map{ rand(5000) }
+  puts "Generating test data"
+  (0..10000).map{ rand(5000) }
 end
 
 # Run 
